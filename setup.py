fcf9dfd40e484808a2725161bda36e60fd068e52
Mohammad <mim.seyedi@gmail.com>
2023-06-23 03:06:37 +0330

    Setup v1.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ dependency_links = ['rich', 'click', 'requests']
 setup (
  name = 'pymg',
  description = 'pymg is a CLI tool that can interpret Python files by the Python interpreter and display the error message in a more readable way if an exception occurs.',
- version = '1.1.0',
+ version = '1.1.1',
  packages = find_packages(),
  install_requires = install_requires,
  python_requires='>=3.11',
@@ -20,7 +20,7 @@ setup (
         pymg=pymg.pymg:main
     ''',
  author="mimseyedi",
- keyword="pymg, debugger, CLI, Python, command-line",
+ keyword=["pymg", "debugger", "CLI", "Python", "bug", "debugger-tool"],
  long_description=README,
  long_description_content_type="text/markdown",
  license='GNU General Public License v3 (GPLv3)',
@@ -30,7 +30,6 @@ setup (
  classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ]
 )
